6b6bf2eabdddc4bd796f34fcb24042887794809e
Nissim Meyer <nissim2305@gmail.com>
2024-04-16 18:13:41 +0300

    update py files

--- task.py
+++ task.py
@@ -40,4 +40,5 @@ def delete_task(number= int):
 
 
 if __name__== '__main__':
-    app.run(port= 5000, debug= True)
+    app.run(port= 5000, host='0.0.0.0', debug= True)
+
--- task_db.py
+++ task_db.py
@@ -2,12 +2,12 @@ import json
 
 
 def get_all_tasks():
-    with open('./tasks-api/tasks_DB.json', 'r') as f:
+    with open('./tasks_DB.json', 'r') as f:
         tasks= json.load(f)
         return tasks
 
 def get_one_task(number: int):
-    with open('./tasks-api/tasks_DB.json', 'r') as f:
+    with open('./tasks_DB.json', 'r') as f:
         tasks= json.load(f)
         for task in tasks:
             if number != task["number"]:
@@ -16,16 +16,16 @@ def get_one_task(number: int):
                 return task
 
 def add_task(new_task):
-    with open('./tasks-api/tasks_DB.json', 'r') as f:
+    with open('./tasks_DB.json', 'r') as f:
         tasks= json.load(f)
     new_task["number"]= max([task["number"] for task in tasks]) + 1
     tasks.append(new_task)
-    with open('./tasks-api/tasks_DB.json', 'w') as f:
+    with open('./tasks_DB.json', 'w') as f:
         json.dump(tasks, f)
     return new_task
 
 def update_task(number: int, task_edit):
-    with open('./tasks-api/tasks_DB.json', 'r') as f:
+    with open('./tasks_DB.json', 'r') as f:
         tasks= json.load(f)
         for task in tasks:
             if number != task["number"]:
@@ -35,18 +35,18 @@ def update_task(number: int, task_edit):
                 task_edit["number"]= old_task["number"]
                 tasks.remove(old_task)
                 tasks.append(task_edit)
-                with open('./tasks-api/tasks_DB.json', 'w') as f:
+                with open('./tasks_DB.json', 'w') as f:
                     json.dump(tasks, f)
                 return task_edit 
 
 def delete_task(number):
-    with open('./tasks-api/tasks_DB.json', 'r') as f:
+    with open('./tasks_DB.json', 'r') as f:
         tasks= json.load(f)
         for task in tasks:
             if number != task["number"]:
                 continue
             else:
                 tasks.remove(task)
-                with open('./tasks-api/tasks_DB.json', 'w') as f:
+                with open('./tasks_DB.json', 'w') as f:
                     json.dump(tasks, f)
                 return 'deleted'
